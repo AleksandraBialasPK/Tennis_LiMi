d8dd58b52bebaf50048fa7e274f7acd8563579f1
Aleksandra <aleksandra.bialas@student.pk.edu.pl>
2024-06-07 00:49:21 +0200

    committing forms

--- Tennis_training_system/Tennis/forms.py
+++ Tennis_training_system/Tennis/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
+from .models import CustomUser, Game, Category, Court, RecurringGroup
+
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('email', 'username', 'password1', 'password2', 'profile_picture')
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Game
+        fields = ['category', 'court', 'start_date_and_time', 'end_date_and_time', 'group']
