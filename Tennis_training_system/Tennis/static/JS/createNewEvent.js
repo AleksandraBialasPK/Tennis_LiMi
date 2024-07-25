ce3b8a8ae3d550306a4e517c07698b2936d3cc12
Aleksandra <aleksandra.bialas@student.pk.edu.pl>
2024-07-25 11:15:05 +0200

    all forms work! Admin view almost ready - change css

--- Tennis_training_system/Tennis/static/JS/createNewEvent.js
+++ Tennis_training_system/Tennis/static/JS/createNewEvent.js
@@ -1,30 +1,33 @@
-const createNewEvent = document.querySelector(".create-new-event"),
-      form = document.getElementById('game_form'),
+const createNewEvent = document.getElementById("create-new-event-button"),
+      addNewCourt = document.getElementById("add-new-court-button")
+      addNewCategory = document.getElementById("create-new-category-button")
+      game_form = document.getElementById('game_form'),
+      court_form = document.getElementById('court_form'),
+      category_form = document.getElementById('category_form'),
       outsideOfForm = document.querySelector("main");
-      categoryField = document.getElementById("id_category");
-      trainerFieldDiv = document.getElementById("id_trainer-div");
 
-if (form) {
-    form.style.display = 'none';
+function toggleForm(form, button) {
+    if (form) {
+        form.style.display = 'none';
 
-    createNewEvent.addEventListener('click', () => {
-        if (form.style.display === 'none') {
-            form.style.display = 'block';
-            outsideOfForm.addEventListener('click', () => {
+        button.addEventListener('click', () => {
+            if (form.style.display === 'none') {
+                form.style.display = 'block';
+                outsideOfForm.addEventListener('click', function hideForm(event) {
+                    if (!form.contains(event.target) && event.target !== button) {
+                        form.style.display = 'none';
+                        outsideOfForm.removeEventListener('click', hideForm);
+                    }
+                });
+            } else {
                 form.style.display = 'none';
-            });
-        } else {
-            form.style.display = 'none';
-        }
-    });
-} else {
-    console.error("Element with ID 'form' not found.");
-}
-
-function checkCategory() {
-    if (categoryField.options[categoryField.selectedIndex].text === "Training") {
-        trainerFieldDiv.style.display = "block";
+            }
+        });
     } else {
-        trainerFieldDiv.style.display = "none";
+        console.error(`Element with ID '${form.id}' not found.`);
     }
 }
+
+toggleForm(game_form, createNewEvent);
+toggleForm(court_form, addNewCourt);
+toggleForm(category_form, addNewCategory);
